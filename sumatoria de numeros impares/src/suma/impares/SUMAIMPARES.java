8d858494fe30463c1c8f07429a394d42ce9de3d9
arleygarzon <60673749+arleygarzon@users.noreply.github.com>
2020-02-04 16:41:56 -0500

    Add files via upload

---
+++ sumatoria de numeros impares/src/suma/impares/SUMAIMPARES.java
@@ -0,0 +1,22 @@
+// ARLEY STEVEN GARZON - INGENIERIA MECATRONICA - 1803664
+package suma.impares;
+
+public class SUMAIMPARES {
+
+    public static void main(String[] args) {
+     long sumatoria=0,b=1;
+System.out.println("Sumatoria de numeros impares del 1 al 100000000");
+while (b<=100000000)
+{
+    sumatoria=sumatoria+b;
+    b=b+2; 
+}
+System.out.println("sumatoria = " + sumatoria);
+    }
+    
+}
+
+
+
+
+
